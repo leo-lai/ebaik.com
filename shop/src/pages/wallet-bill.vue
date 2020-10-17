80bbb1daa8017bc0450c632fe159a85eab28c6ac
leolai <leolai.mail@qq.com>
2020-10-17 12:21:51 +0800

    消费记录

--- shop/src/pages/wallet-bill.vue
+++ shop/src/pages/wallet-bill.vue
@@ -7,20 +7,26 @@
     </header>
 
     <div class="l-bg-white">
-      <x-table>
+      <x-table class="l-fs-xs">
         <thead>
           <tr>
-            <th class="l-padding-lr-s" width="140">时间</th>
-            <th class="l-padding-lr-s">商品</th>
-            <th class="l-padding-lr-s">价格</th>
+            <th class="l-padding-lr-s" width="80">时间</th>
+            <th class="l-padding-lr-s">商品信息</th>
+            <th class="l-padding-lr-s">支付类型</th>
             <th class="l-padding-lr-s">实付</th>
           </tr>
         </thead>
         <tbody class="l-fs-xs">
           <tr v-for="item in list.data" :key="item.orderId">
-            <td class="l-fs-xs">{{item.createTime}}</td>
-            <td>{{item.goodsInfo[0]}}</td>
-            <td><i class="l-rmb">{{item.goodsInfo[1]}}</i></td>
+            <td class="l-word">{{item.createTime}}</td>
+            <td>
+              <p v-for="goods in item.goodsList" :key="goods.id">
+                <span class="l-margin-r-s">{{goods.name}}</span>
+                <span class="l-margin-r-s">{{goods.price}}</span>
+                <span> x{{goods.number}}</span>
+              </p>
+            </td>
+            <td>{{paymentType[item.paymentType]}}</td>
             <td><i class="l-rmb">{{item.actualPayment}}</i></td>
           </tr>
         </tbody>
@@ -48,6 +54,16 @@ export default {
   },
   data () {
     return {
+      paymentType: {
+        '-1': '其它支付',
+        '0': '微信支付',
+        '1': '支付宝支付',
+        '2': '储值支付',
+        '3': '扫呗微信',
+        '4': '扫呗支付宝',
+        '24': '礼券支付',
+        '25': '储存金支付',
+      },
       list: {
         filter: {
           beginTime: '',
@@ -80,7 +96,15 @@ export default {
       this.list.loading = true
       this.$ajax.json('/sys-statementcenter/orderCenter/findCustomerOrder', formData).then(({ data }) => {
         this.list.data = data.list.map(item => {
-          item.goodsInfo = item.goodsNamePriceNumber.split(':')
+          item.goodsList = item.goodsNamePriceNumber.split(',').map(goodsInfo => {
+            goodsInfo = goodsInfo.split(':')
+            let goods = {}
+            goods.name = goodsInfo[0]
+            goods.price = goodsInfo[1]
+            goods.number = goodsInfo[2]
+            goods.id = goodsInfo[3]
+            return goods
+          })
           return item
         })
         this.list.pager.total = data.total
@@ -101,6 +125,6 @@ export default {
 </script>
 
 <style lang="less">
-
+.l-word{ line-height: 1.2; word-break: break-word; white-space: inherit; }
 </style>
 
