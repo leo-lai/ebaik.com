7a3cd28a8c9aea0ac068ceabee1735731feff9b1
leolai <leolai.mail@qq.com>
2020-10-29 23:27:05 +0800

    qcsy改界面

--- shop/src/pages/pay.vue
+++ shop/src/pages/pay.vue
@@ -147,7 +147,6 @@ export default {
           openid: this.openId
         }).then(({ data }) => {
           if(data.code === 0 && data.appId) {
-            let that = this
             let payConfig = {
               'appId': data.appId,          // 公众号名称，由商户传入
               'timeStamp': data.timeStamp,  // 时间戳，自1970年以来的秒数
@@ -156,13 +155,13 @@ export default {
               'signType': data.signType,  // 微信签名方式：
               'paySign': data.paySign     // 微信签名,paySign 采用统一的微信支付 Sign 签名生成方法，注意这里 appId 也要参与签名，appId 与 config 中传入的 appId 一致，即最后参与签名的参数有appId, timeStamp, nonceStr, package, signType。
             };
-            this.$onBridgeReady(function() {
-              WeixinJSBridge.invoke('getBrandWCPayRequest', payConfig, function (res) {
+            this.$onBridgeReady(() => {
+              WeixinJSBridge.invoke('getBrandWCPayRequest', payConfig, (res) => {
                 // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
                 if (res.err_msg.indexOf('get_brand_wcpay_request:ok') > -1) { 
-                  that.$router.replace('/pay/result')
+                  this.$router.replace('/pay/result')
                 } else if(res.err_msg.indexOf('cancel') === -1){
-                  that.$api.alert(res.err_msg)
+                  this.$api.alert(res.err_msg)
                 }
               })
             })
--- shop/src/pages/qcsy/recharge-1.vue
+++ shop/src/pages/qcsy/recharge-1.vue
@@ -109,7 +109,6 @@ export default {
       this.$api.loading.show()
       this.$ajax.json('/sys-machinepurchase/recharge/rechargeOrder', formData).then(({ data }) => {
         if(data.appId) {
-          let that = this
           let payConfig = {
             'appId': data.appId,          // 公众号名称，由商户传入
             'timeStamp': data.timeStamp,  // 时间戳，自1970年以来的秒数
@@ -118,16 +117,16 @@ export default {
             'signType': data.signType,  // 微信签名方式：
             'paySign': data.paySign     // 微信签名,paySign 采用统一的微信支付 Sign 签名生成方法，注意这里 appId 也要参与签名，appId 与 config 中传入的 appId 一致，即最后参与签名的参数有appId, timeStamp, nonceStr, package, signType。
           };
-          this.$onBridgeReady(function() {
-            WeixinJSBridge.invoke('getBrandWCPayRequest', payConfig, function (res) {
+          this.$onBridgeReady(() => {
+            WeixinJSBridge.invoke('getBrandWCPayRequest', payConfig, (res) => {
               // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
               if (res.err_msg.indexOf('get_brand_wcpay_request:ok') > -1) { 
-                that.$api.toast('充值成功')
-                // that.$router.replace('/purchase?tab=1')
+                this.$api.toast('充值成功')
+                // this.$router.replace('/purchase?tab=1')
                 // this.$href(this.$utils.url.getFullPath('.') + '?#/purchase?tab=1', 'replace')
                 this.getUserInfo()
               } else if(res.err_msg.indexOf('cancel') === -1){
-                that.$api.alert(res.err_msg)
+                this.$api.alert(res.err_msg)
               }
             })
           })
